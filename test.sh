da0ca9aec425701008630dbb4f4a1bbe27e90eb6
filippo <filippo5.alessio5@gmail.com>
2025-01-02 17:15:16 +0100

    fix readme

--- test.sh
+++
@@ -1,7 +0,0 @@
-serverKeyFile=$(echo "192.168.157.110-rvvmb3901pl,psm.regione.veneto.it,22,filippo-farinazzo@root@192.168.157.110,dvb8mfb2wbt1dzkXWF,,," | awk -F, '{print $6}')
-
-if [ -z $serverKeyFile]
-then 
-echo "ciao" 
-fi
-
